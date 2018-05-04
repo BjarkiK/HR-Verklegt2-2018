f28c0b850345fb3da467324cd8cd142bf52c0d12
Júlía Ýr Þovaldsdóttir <Juliayr@eduroam.nat.ru.is>
2018-05-04 21:26:21 +0000

    Fix typeId repo and viewmodel

--- Data/EntityModels/Order.cs
+++ Data/EntityModels/Order.cs
@@ -9,7 +9,7 @@ namespace TheBookCave.Data.EntityModels {
         // Date when order was made.
         public DateTime Date { get; set; }
         // Genre id of order status (Finished, canceled, unfinsished...).
-        public int GenreId { get; set; }
+        public int TypeId { get; set; }
         // Id of address shipped to.
         public int AddressId { get; set; }
 
