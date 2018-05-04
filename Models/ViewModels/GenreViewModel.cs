2ed7cf965506b080d5d3d0209f62bde7213a0ac3
Júlía Ýr Þovaldsdóttir <Juliayr@eduroam.nat.ru.is>
2018-05-04 14:46:43 +0000

    added genre ViewModel class

---
+++ Models/ViewModels/GenreViewModel.cs
@@ -0,0 +1,10 @@
+namespace TheBookCave.Models.ViewModels {
+    public class GenreViewModel {
+        public int Id { get; set; }
+        // The name of genre in Icelandic.
+        public int GenreIs { get; set; }
+        // The name of genre in English.
+        public int GenreEn { get; set; }
+
+    }
+}
