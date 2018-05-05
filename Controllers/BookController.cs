65bd768da8de4cfabbc38251b31562b771581038
Thorlaugarson <gudmundurht16@ru.is>
2018-05-05 21:29:09 +0000

    minor change

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -25,11 +25,7 @@ namespace TheBookCave.Controllers
 
         public IActionResult Details(int id)
         {
-<<<<<<< HEAD
-            var book = _bookService.getBook(bid);
-=======
-            var book = _bookService.GetBook(id);
->>>>>>> 833784ddc8823f06b23d6d6245dffb2579c16170
+            var book = _bookService.getBook(id);
             return View(book);
             
         }
