95de6a1be85546dee5c2d560eb7f5388bd2391b5
Ingi <ingi17@ru.is>
2018-05-08 14:26:50 +0000

    DB - Default Vale

---
+++ Migrations/20180508142249_[Grade]To[Grade]In[Book]Migration_defaultValueUpdate.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace TheBookCave.Migrations
+{
+    public partial class GradeToGradeInBookMigration_defaultValueUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Grade",
+                table: "Books",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Grade",
+                table: "Books",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
